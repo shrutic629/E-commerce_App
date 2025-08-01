c751951ec65c7a26039ae4e03e6391bdb43fd848
shrutic629 <shrutic629@gmail.com>
2025-08-01 14:44:26 +0530

    added

--- Backend/controllers/sellerController.js
+++ Backend/controllers/sellerController.js
@@ -1,9 +1,37 @@
 const mongoose = require('mongoose')
 const Seller = require('../models/sellerModel')
+const Otp =require('../models/otpModel')
+const sendEmail = require('../utils/sendEmail')
 
 exports.signUp = async(req,res,next)=>{
     try{
         const {firstName,lastName,email,password,phoneCountryCode,phoneNumber} = req.body;
+        if(!(firstName && lastName && email && password && phoneCountryCode && phoneNumber)){
+            return res.status(400).json({message:'All fields are required'})
+        }
+
+        const existingSeller = await Seller.findOne({email})
+        if(existingSeller){
+            return res.status(400).json({message:"Seller already exist"})
+        }
+
+        const seller = new Seller({firstName,lastName,email,password,phoneCountryCode,phoneNumber})
+        await seller.save()
+
+        await generateAndSendOtp({
+            purpose: 'signup',
+            userType: 'Seller',
+            userId: seller._id,
+            mode: 'email',
+            email,
+            phoneCountryCode,
+            phoneNumber
+        })
+
+        const duplicateSeller = seller.toObject()
+        delete duplicateSeller.password
+
+        res.status(200).json({message:"Seller created successfully",seller:duplicateSeller})
     }
     catch(err){
         next(err)
@@ -12,7 +40,13 @@ exports.signUp = async(req,res,next)=>{
 
 exports.confirmOtp = async(req,res,next)=>{
     try{
+        const {otp,email} = req.body
 
+        if(!(otp && email)){
+            return res.status(400).json({message:"All fields are required"})
+        }
+
+        
     }
     catch(err){
         next(err)
